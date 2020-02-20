5c6aa0b6e021544316ae7653f41e28f3936fe284
郝东生 <haodongsheng@haodongshengdeMacBook-Air.local>
2020-02-20 17:40:37 +0800

    修复安卓状态栏存在的半透明问题

--- same_netease_music/lib/main.dart
+++ same_netease_music/lib/main.dart
@@ -13,8 +13,19 @@ import 'model/PersonalizedInfo.dart';
 //import 'package:extended_image/extended_image.dart';
 import 'controller/play.dart';
 import 'controller/listDetail.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+  if (Platform.isAndroid) {
+    // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
+    SystemUiOverlayStyle systemUiOverlayStyle =
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+}
+
 Map<String, WidgetBuilder> routers = {
   '/play': (context, {arguments}) => PlayPage(arguments),
   '/more': (context, {arguments}) => MorePage(),
